2c13151ec6dd3c2ee06d1b285b19209dc0a7db7c
Fabien Winkler <fabien.winkler@outlook.fr>
2021-04-25 22:20:58 +0200

    [Feat] binding / closing modal

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
+import "./style.css";
 import { FabModal } from "./core/FabModal";
 import { FabModalManager } from "./core/FabModalManager";
+
+// For disable error TS -> Property 'X' does not exist on type 'Window'
+declare global {
+  interface Window {
+    modalManager: FabModalManager;
+    modal: FabModal;
+  }
+}
 
 window.modalManager = new FabModalManager();
 window.modal = new FabModal();
