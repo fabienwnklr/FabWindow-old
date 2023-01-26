ffc71bea6fb4098688377c8089fa4d5245810988
Fabien Winkler <fabien.winkler@outlook.fr>
2023-01-26 21:22:32 +0100

    fix: rm temp blog part

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -93,7 +93,7 @@ const config = {
             position: "left",
             label: "API Documentation",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          // { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/facebook/docusaurus",
             label: "GitHub",
